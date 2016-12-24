94e120e1219a0ba18e2ef6de0f6ab189c2bd0067
Shahin Zangenehpour <shahin@98chimp.com>
2016-12-23 23:24:13 -0800

    prepared a simple pie chart

--- ChartsDemo/ViewController.swift
+++ ChartsDemo/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import Charts
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ChartViewDelegate
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        title = "The Best Pie Chart Ever"
+        
+        setupPieChartView(pieChartView)
+        
+        pieChartView.delegate = self
+        
+        updateChartData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupPieChartView(_ chartView: PieChartView)
+    {
+        chartView.usePercentValuesEnabled = true
+        chartView.drawSlicesUnderHoleEnabled = false
+        chartView.holeRadiusPercent = 0.50
+        chartView.transparentCircleRadiusPercent = 0.53
+        chartView.chartDescription?.enabled = false
+        chartView.setExtraOffsets(left: 5, top: 10, right: 5, bottom: 5)
+        chartView.drawCenterTextEnabled = true
+        
+        chartView.drawHoleEnabled = true
+        chartView.rotationAngle = 0.0
+        chartView.rotationEnabled = true
+        chartView.highlightPerTapEnabled = true
+        
+        let legend = chartView.legend
+        legend.horizontalAlignment = .right
+        legend.verticalAlignment = .top
+        legend.orientation = .vertical
+        legend.drawInside = false
+        legend.xEntrySpace = 7.0
+        legend.yEntrySpace = 0.0
+        legend.yOffset = 0.0
     }
+    
+    func updateChartData()
+    {
+        var colors = [UIColor]()
+        colors.append(UIColor.blue)
+        colors.append(UIColor.red)
+        colors.append(UIColor.orange)
+        colors.append(UIColor.green)
+        
+        var values = [PieChartDataEntry]()
+        values.append(PieChartDataEntry(value: 13.5, label: "Thing 1"))
+        values.append(PieChartDataEntry(value: 18.5, label: "Thing 2"))
+        values.append(PieChartDataEntry(value: 48.2, label: "Thing 3"))
+        values.append(PieChartDataEntry(value: 19.8, label: "Thing 4"))
 
-
+        let dataset = PieChartDataSet(values: values, label: "Things of the World")
+        
+        dataset.colors = colors
+        
+        let data = PieChartData(dataSet: dataset)
+        
+        pieChartView.data = data
+    }
+    
 }
 
