d1bf9934b7a3b03b56d941827689c63faa5bcbb3
Shahin Zangenehpour <shahin@98chimp.com>
2016-12-23 17:49:51 -0800

    Initial Commit

---
+++ ChartsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChartsDemo
+//
+//  Created by Shahin on 2016-12-23.
+//  Copyright © 2016 98%Chimp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
